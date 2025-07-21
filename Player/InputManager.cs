ddc853052f11940d70a788c7fb82745f26f5695d
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-07-21 23:17:45 +0200

    Restart input fix

--- Player/InputManager.cs
+++ Player/InputManager.cs
@@ -19,9 +19,12 @@ public class InputManager : MonoBehaviour
         inputs.Player.Attack.performed += OnAttack;
         inputs.Player.Move.performed += OnMove;
         inputs.Player.Look.performed += OnLook;
+        inputs.Player.Restart.performed += OnRestart;
+
         inputs.Player.Attack.Enable();
         inputs.Player.Move.Enable();
         inputs.Player.Look.Enable();
+        inputs.Player.Restart.Enable();
     }
     public void OnLook(InputAction.CallbackContext obj)
     {
@@ -39,7 +42,7 @@ public class InputManager : MonoBehaviour
         Debug.Log("OnAttack");
         LeftMouse?.Invoke();
     }
-    public void OnRestart()
+    public void OnRestart(InputAction.CallbackContext obj)
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
